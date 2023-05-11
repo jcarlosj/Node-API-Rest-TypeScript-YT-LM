a1b94ece02b908ec1bf4de628218dec3ab0cefd4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-05-11 11:11:01 -0500

    [+] router/item: [GET] /items [+] app.ts: import router/item

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import { router } from './routes/item';
+
 const 
     PORT = process.env.PORT || 3001,
     app = express();
 
 // Middlewares
 app.use( cors() );      // Cross-Origin Resource Sharing (CORS)
+app.use( router );      // Item Router
 
 app.listen( PORT, () => console.log( `Escuchando por el puerto ${ PORT }` ) );
