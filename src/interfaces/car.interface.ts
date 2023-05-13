98caf584c8d3cb5efd394ae977b7c12ae93c18ff
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-05-13 13:02:04 -0500

    [+] interfaces/items: Car

---
+++ src/interfaces/car.interface.ts
@@ -0,0 +1,9 @@
+export interface Car {
+    brand: string;
+    color: string;
+    description: string;
+    gas: 'gasoline' | 'electric';
+    model: string;
+    price: number;
+    year: number;
+} 
