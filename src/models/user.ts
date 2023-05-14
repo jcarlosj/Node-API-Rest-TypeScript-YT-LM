8f35b5c767d2710b60bb0dde25ff3d349fee018f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-05-14 12:15:45 -0500

    [+] models/users: Schema & Model

---
+++ src/models/user.ts
@@ -0,0 +1,35 @@
+import { Schema, Types, Model, model } from 'mongoose';
+import { User } from '../interfaces/user.interface';
+
+
+// * Schema: Es una representacion estructural de la entidad que se va a guardar en la base de datos
+const authSchema = new Schema<User>({
+    email: {
+        type: 'string',
+        required: true,
+        unique: true
+    },
+    password: {
+        type: 'string',
+        required: true
+    },
+    name: {
+        type: 'string',
+        required: true
+    },
+    description: {
+        type: 'string',
+        default: 'User description'
+    }
+},
+{
+    timestamps: true,
+    versionKey: false,
+});
+
+
+// * Creamos un modelo a partir de nuestro Schema
+const userModel = model( 'users', authSchema );
+
+
+export default userModel;
