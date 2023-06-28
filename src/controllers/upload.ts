22c0879b95c2d9c5a7160cbb9577ecf7f534f2ee
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-28 10:43:01 -0500

    [+] upload.controller: Obtiene datos del usuario y archivo subido

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -5,9 +5,15 @@ import { RequestExtends } from '../interfaces/RequestExtends.interface';
 
 const getFile = async ( req: RequestExtends, res: Response ) => {
     try {
+        const { authUser, file } = req;
+
+        console.log({ authUser: authUser ? authUser : 'NOT FOUND USER' });
+        console.log({ file });
+
         res.send({ 
-            msg: `Aqui debe cargar el archivo`,
-            user: req.authUser 
+            msg: `Aqui debe cargar el archivo, sus detalles y propietario`,
+            file,
+            user: authUser ? authUser : 'NOT FOUND USER'
         });
     }
     catch( error ) {
